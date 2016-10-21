8deb6bcb323aee9dc2640f4b273064319724973c
sarathprasath <sarath@freshdesk.com>
2016-10-22 01:03:43 +0530

    CRUD Operations Complete on Todo

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express();
 
 app.use(orm.express("mysql://root:@localhost/minions", {
   define: function (db, models, next) {
-    console.log('Hello');
     models.todo = db.define("todo", {
       id: { type: 'number', key: true },
+      account_id: {type: 'number'},
       ticket_id: {type: 'number' },
-      content: { type: 'text', size: 255 },
       user_id: { type: 'number'},
+      content: { type: 'text', size: 255 },
       completed: {type: 'boolean', defaultValue: false },
       created_at: { type: 'date', time: true },
       updated_at: { type: 'date', time: true }
@@ -30,14 +30,59 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/todo/', function (req, res) {
+app.post('/todos/', function (req, res) {
   req.models.todo.create({
-      ticket_id: req.body.ticket_id,
+      ticket_id: req.body.ticket_id, account_id: req.body.account_id,
       content: req.body.content, user_id: req.body.user_id,
       created_at: new Date(), updated_at: new Date()
     },function(err, todo){
-      if(err) throw err;
-      console.log('Successfully created a todo');
-      res.send(todo);
+      if(err) { console.log('Error Creating todo', err); }
+      res.json(todo);
+  });
+});
+
+app.get('/todos/:id', function(req, res){
+  req.models.todo.get(req.params.id, function(err, todo){
+    if(err) {
+      console.log('Error Fetching Todo', err);
+    }
+    res.json(todo);
+  });
+});
+
+app.get('/todos/:account_id/:ticket_id', function(req, res){
+  req.models.todo.find({
+    account_id: req.params.account_id, ticket_id: req.params.ticket_id
+  }, function(err, todos){
+    if(err) {
+      console.log('Error Fetching Todo', err);
+    }
+    res.json(todos);
+  });
+});
+
+app.patch('/todos/:id', function(req,res){
+  req.models.todo.get(req.params.id, function(err, todo) {
+    if(err){
+      console.log('Error Fetching Todo', err);
+    }
+    if(req.body.content){
+      todo.content = req.body.content;
+    }
+    if(req.body.completed) {
+      todo.completed = req.body.completed;
+    }
+    todo.save();
+    res.json(todo);
+  });
+});
+
+app.delete('/todos/:id',function(req, res){
+  req.models.todo.get(req.params.id, function(err, todo){
+    if(err){
+      console.log('error while deleting todo');
+    }
+    todo.remove();
+    res.json(todo);
   });
 });
