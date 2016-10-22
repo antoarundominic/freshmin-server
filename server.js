696b9c0148069b96fe40b60e71e4afc70b044112
Gopishankar <gopishankar@freshdesk.com>
2016-10-22 06:53:48 +0530

    Adding Due date functionality

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var orm = require("orm");
 var util = require('util');
 var bodyParser = require('body-parser');
+// var lt = require('long-timeout');
+var kue = require('kue')
+ , queue = kue.createQueue();
 var app = express();
 
 app.use(orm.express("mysql://root:@localhost/minions", {
@@ -13,6 +16,8 @@ app.use(orm.express("mysql://root:@localhost/minions", {
       user_id: { type: 'number'},
       content: { type: 'text', size: 255 },
       completed: {type: 'boolean', defaultValue: false },
+      due_date: { type: 'date', time: true },
+      job_id: { type: 'number' },
       created_at: { type: 'date', time: true },
       updated_at: { type: 'date', time: true }
     });
@@ -31,11 +36,31 @@ app.get('/', function (req, res) {
 });
 
 app.post('/todos/', function (req, res) {
+  var due_date = new Date(req.body.due_date)- (330*60*1000);
   req.models.todo.create({
-      ticket_id: req.body.ticket_id, account_id: req.body.account_id,
-      content: req.body.content, user_id: req.body.user_id,
-      created_at: new Date(), updated_at: new Date()
+      ticket_id: req.body.ticket_id,
+      account_id: req.body.account_id,
+      content: req.body.content,
+      user_id: req.body.user_id,
+      due_date: new Date(due_date),
+      created_at: new Date(),
+      updated_at: new Date()
     },function(err, todo){
+        var milliseconds = todo.due_date - (new Date()).getTime();
+        var job = queue.create('notify'+todo.id, 
+                  { todo_id: todo.id })
+                  .delay(milliseconds)
+                  .save(function(err){
+                      todo.job_id = job.id;
+                      todo.save();
+                  });
+
+      //Timer expires and we need to send notification from here
+      //Hook to send notification
+      queue.process('notify'+todo.id, function(job, done) {
+        console.log("Notify the user on due date");
+      });
+
       if(err) { console.log('Error Creating todo', err); }
       res.json(todo);
   });
@@ -71,6 +96,17 @@ app.patch('/todos/:id', function(req,res){
     }
     if(req.body.completed) {
       todo.completed = req.body.completed;
+      console.log("inside completed");
+      //Undo logic should be thought.
+      //Deletion logic
+      kue.Job.get( todo.job_id, function( err, job ) {
+      // change job properties
+        if (err) return;
+        console.log("Job Id : "+ job.id);
+        job.remove(function(err,job){
+          console.log('removed completed job #%d', job.id);
+        });
+      });
     }
     todo.save();
     res.json(todo);
